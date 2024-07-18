8a81568b64d55ac61263f7af239fb9663281265b
Zakariae El Messoudi <zelmessoudi@smb.sqli.com>
2024-07-18 10:31:35 +0100

    feat: add search bar input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from '@containers/index';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'search-bar';
---
+++ src/app/containers/search/search.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SearchComponent {
+  search = '';
+  searchChannel = new BroadcastChannel('search-channel');
+  
+  submitSearch() {
+    this.searchChannel.postMessage(this.search);
+  }
+}
